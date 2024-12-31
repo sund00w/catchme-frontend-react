6e9321509621c02dfb44e297bfc0974eef7c62a2
sund00w <dwsun212@gmail.com>
2024-12-31 21:00:55 +0900

    feat: test component 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import { TestComponent } from './component/test-component';
+import { TestComponent2 } from './component/test-component2';
+
 function App() {
+  console.log("App이 재호출되었습니다.");
   return (
-    <div className="App">
-      <div>hello</div>
+    <div className='App'>
+      <TestComponent />
+      <TestComponent2 />
     </div>
   );
 }
